4ef40f830385463da1d446f66f2760c01e730da9
Atusia <adriannakrol@student.agh.edu.pl>
2023-12-29 15:56:54 +0100

    Wykresy i tabelki że mucha nie siada

--- Heart Plotter - App/HeartPlotterApp.cpp
+++ Heart Plotter - App/HeartPlotterApp.cpp
@@ -1,13 +1,13 @@
 #include "HeartPlotterApp.h"
 #include "qcustomplot/qcustomplot.h"
 
-HeartPlotterApp::HeartPlotterApp(QWidget *parent)
+HeartPlotterApp::HeartPlotterApp(QWidget* parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
 
     // Create a QCustomPlot widget
-    QCustomPlot *customPlot = new QCustomPlot(this);
+    QCustomPlot* customPlot = new QCustomPlot(this);
 
     // Enable scroll bars for the plot's viewport
     customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
@@ -18,8 +18,8 @@ HeartPlotterApp::HeartPlotterApp(QWidget *parent)
 
     // Legend
     customPlot->legend->setVisible(true);
-    QFont legendFont = font();  
-    legendFont.setPointSize(9); 
+    QFont legendFont = font();
+    legendFont.setPointSize(9);
     customPlot->legend->setFont(legendFont);
     customPlot->legend->setBrush(QBrush(QColor(255, 255, 255, 230)));
 
@@ -28,13 +28,14 @@ HeartPlotterApp::HeartPlotterApp(QWidget *parent)
     xData << 1 << 2 << 3; yData << 1 << 4 << 9;
 
     customPlot->addGraph();
-    customPlot->graph(0)->setPen(QPen(Qt::blue)); 
+    customPlot->graph(0)->setPen(QPen(Qt::blue));
     customPlot->graph(0)->setData(xData, yData);
     customPlot->graph(0)->setName("Data");
 
+
     // Create a graph 1 and set data
     customPlot->addGraph();
-    customPlot->graph(1)->setPen(QPen(Qt::red)); 
+    customPlot->graph(1)->setPen(QPen(Qt::red));
     customPlot->graph(1)->setLineStyle(QCPGraph::lsNone);
     customPlot->graph(1)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 7));
     customPlot->graph(1)->setData(xData, yData);
@@ -63,19 +64,21 @@ HeartPlotterApp::HeartPlotterApp(QWidget *parent)
     customPlot->rescaleAxes();
     customPlot->replot();
 
-    // Add customPlot to the UI
-    QWidget *plot_container =  ui.plotContainer;
-    QVBoxLayout *plot_layout = new QVBoxLayout(plot_container);
-    plot_layout->addWidget(customPlot);
-    plot_layout->addWidget(dataTable);
+    // Create a main layout
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
 
-    // Ensure the layout updates when the window is resized
-    plot_layout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    // Add the QCustomPlot and QTableWidget to the main layout
+    mainLayout->addWidget(customPlot);
+    mainLayout->addWidget(dataTable);
 
-    // Adjust layout and size policy
-    plot_layout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
-    customPlot->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    // Set stretch factors to control how the available space is distributed
+    mainLayout->setStretchFactor(customPlot, 2);  // Adjust the stretch factors as needed
+    mainLayout->setStretchFactor(dataTable, 1);
 
+    // Set the main layout for the central widget
+    QWidget* centralWidget = new QWidget(this);
+    centralWidget->setLayout(mainLayout);
+    setCentralWidget(centralWidget);
 }
 
 HeartPlotterApp::~HeartPlotterApp() = default;
