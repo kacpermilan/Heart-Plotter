78ea42c660fc8af64e2594bffde4ac1eb58328dd
kacpermilan <kacpermmilan@gmail.com>
2024-01-09 18:56:15 +0100

    fix: remove the unneccesary files and add example files

--- Heart Plotter - App/main.cpp
+++ Heart Plotter - App/main.cpp
@@ -1,10 +1,10 @@
-#include "Output.h"
+#include "HeartPlotterApp.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Output w;
+    HeartPlotterApp w;
     w.show();
     return a.exec();
 }
